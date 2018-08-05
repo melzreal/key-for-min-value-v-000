8de82988195d864d57fb7c848009e47dcdb9a802
Melissa Real <melissa.real@hotmail.com>
2018-08-05 14:09:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,19 +1,15 @@
-def key_for_min_value(name_hash)
+def key_for_min_value(name_hash) 
   min = nil
-  min_k = nil
+  min_k = nil 
 
-  name_hash.each do |key, value|
-
-    if min ==nil
-    min = value
-    min_k = key
-    elsif min > value
-      min = value
-      min_k = key
+  name_hash.each do |key, value| 
+    if min == nil
+    min = value 
+    min_k = key 
+    elsif min > value 
+      min = value 
+      min_k = key 
     end
-
-  end
-
+  end 
   min_k
-
 end
